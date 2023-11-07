f3b743d96f7c454949d37401b5349507436cb133
FabezoN <dela.noe@hotmail.fr>
2023-11-07 15:38:55 +0100

    FirstCommit

---
+++ pages/HomesPage.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export function HomePage() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+  
