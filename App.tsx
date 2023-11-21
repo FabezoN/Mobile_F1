f73cfef147022d4c611b0e709537f86a992bf55f
FabezoN <dela.noe@hotmail.fr>
2023-11-21 15:55:22 +0100

    Constructors page with item (image,postion,name)

--- App.tsx
+++ App.tsx
@@ -8,7 +8,6 @@ import { Tracks } from './src/pages/Tracks';
 import {QueryClient,QueryClientProvider} from '@tanstack/react-query'
 import { Constructors } from './src/pages/Constructors';
 
-
 const Tab = createBottomTabNavigator();
 
 const queryClient = new QueryClient(({
@@ -24,10 +23,10 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomePage} />
-        <Tab.Screen name="Drivers" component={Drivers} />
-        <Tab.Screen name="Tracks" component={Tracks} />
-        <Tab.Screen name="Constructors" component={Constructors} />
+        <Tab.Screen name="Home" component={HomePage} options={{headerShown:false}} />
+        <Tab.Screen name="Drivers" component={Drivers} options={{headerShown:false}}/>
+        <Tab.Screen name="Tracks" component={Tracks} options={{headerShown:false}} />
+        <Tab.Screen name="Constructors" component={Constructors} options={{headerShown:false}} />
       </Tab.Navigator>
     </NavigationContainer>
     </QueryClientProvider>
--- src/pages/Constructors.tsx
+++ src/pages/Constructors.tsx
@@ -1,24 +1,48 @@
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView,Image, ImageRequireSource } from 'react-native';
 import { useGetProducts } from '../hooks/useGetConstructors';
 
 
-export function Constructors() {
+const ConstructorImagesMapping: Record<string, ImageRequireSource> = {
+  'red_bull': require('../../assets/Constructors/red_bull.png'),
+  'mercedes': require('../../assets/Constructors/mercedes.png'),
+  'ferrari': require('../../assets/Constructors/ferrari.png'),
+  'mclaren': require('../../assets/Constructors/mclaren.png'),
+  'aston_martin': require('../../assets/Constructors/astonmartin.png'),   
+  'alpine': require('../../assets/Constructors/alpine.png'), 
+  'williams': require('../../assets/Constructors/williams.png'),
+  'alphatauri': require('../../assets/Constructors/alphatauri.png'),
+  'alfa': require('../../assets/Constructors/alfaromeo.png'),
+  'haas': require('../../assets/Constructors/haas.png'),
+}
+
+export function Constructors({ navigation }: any) {
   const {data} = useGetProducts();
-  console.log({data})
+  const onPress = () => {
+    navigation.navigate('Drivers')
+  }
     return (
       <ScrollView style={styles.main}>
         <Text style={styles.constructors}>Constructors</Text>
-        <TouchableOpacity style={styles.textDrivers}><Text style={{color: 'white', textAlign: 'center',borderTopColor: 'white',borderTopWidth: 1}}>Drivers</Text></TouchableOpacity>
-        <TouchableOpacity onPress={Constructors}>
+        <View style= {styles.button}>
+        <TouchableOpacity onPress ={onPress} style={styles.boxDrivers}>
+          <Text style={styles.textDrivers}>Drivers</Text>
+          </TouchableOpacity>
+        <TouchableOpacity  style={styles.boxConstructors}>
           <Text style={styles.textConstructors}>Constructors</Text>
         </TouchableOpacity>
+        </View>
         {data?.MRData.StandingsTable.StandingsLists[0].ConstructorStandings.map((item, i) => (
         <View style={styles.view} >
-          <Text style={styles.position} >{item.position}{"\n"}</Text>
-          <Text style={styles.Name} key={i}>{item.Constructor.name}</Text>
-          <Text style={styles.points} key={i}>{item.points}</Text>
-          <Text style={styles.wins} >Race wins: {item.wins}</Text>
-        </View>
+          <View style={styles.textView}>
+            <Text style={styles.position} >{item.position}{"\n"}</Text>
+            <Text style={styles.Name}>{item.Constructor.name}</Text>
+            <Text style={styles.wins} >Race wins: {item.wins}</Text>
+          </View>
+          <View style={styles.ImageView}>
+              <Image style={styles.imageConstructors} source={ConstructorImagesMapping[item.Constructor.constructorId]} />
+              <Text style={styles.points} >{item.points}{' PTS'}</Text>
+           </View>
+        </View >
         ))}
       </ScrollView>
     );
@@ -33,68 +57,107 @@ export function Constructors() {
     constructors: {
       fontWeight: '700',
       color: 'white', 
-      fontSize: 34, 
-      top: 71, 
-      left: 27
+      fontSize: 41, 
+      paddingTop: 71,
+      paddingLeft: 27,
     },
+    button:{
+      flex:1,
+      paddingTop:32,
+      display: 'flex',
+      flexDirection: 'row',
+      paddingRight: 27,
+      paddingLeft: 27,
+      justifyContent: 'space-between',
+      paddingBottom: 24,
+    },
+
     textDrivers: {
-      fontWeight: '700',
+      textAlign: 'center',
       color: 'white', 
-      top: 156, 
-      left: 27, 
-      width: 160,
-      textAlign: 'center', 
-      fontSize: 15
+      fontWeight: '700',
+      fontSize: 15,
+    },
+    boxDrivers:{
+      flex: 1,height: 44,
+      borderTopWidth: 5,
+      borderRightWidth: 5,
+      justifyContent: 'center',
     },
     textConstructors : {
-      fontWeight: '700',
-      opacity: 0.5,
-      color: 'white', 
-      top: 140, 
-      left: 203, 
-      width: 160,
-      textAlign: 'center', 
-      fontSize: 15
-    },
-    view : {
-      width: 336, 
-      height: 148, 
-      top: 188, 
-      backgroundColor: '#1B1A19', 
-      left: 25, 
-      marginBottom: 15, 
-      borderRadius: 14
-    },
-    position : {
       color: 'white',
-      left: 20,
-      top: 15, 
-      fontSize: 20, 
-      fontWeight: '700'
+      fontWeight: '700',
+      fontSize: 15,
+      textAlign: 'center',
     },
+
+    boxConstructors : {
+      borderTopRightRadius: 15,
+      borderColor: 'red',
+      borderTopWidth: 5,
+      borderRightWidth: 5,
+      flex: 1,
+      height: 44,
+      justifyContent: 'center',
+    },
+
+    view : {
+      height: 148, 
+      backgroundColor: '#1B1A19', 
+      marginLeft: 27,
+      marginRight: 27,
+      marginBottom: 16, 
+      borderRadius: 14,
+      display: 'flex',
+      flexDirection: 'row',
+    },
+    
+    textView : {
+      flex : 1,
+      paddingRight: 46,
+      paddingLeft: 20,
+  
+    },
+
+    position : {
+      color: 'white', 
+      fontSize: 20, 
+      fontWeight: '700',
+      paddingTop: 20,
+    },
+
     Name : {
       color: 'white',
-      left: 20,
-      top: 15, 
       fontSize: 20, 
-      fontWeight: '700'
+      fontWeight: '700',
+      height: 35,
     },
+
     points : {
       color: 'white',
       fontSize: 20,
-      left: 226,
-      bottom: 125,
-      textAlign: 'center' ,
-      width: 80, 
+      textAlign: 'center',
       fontWeight: '700',
     },
     wins : {
       color: '#CCCDD7',
-      left: 20,
-      top: 15, 
       fontSize: 11, 
       width: 90, 
-      fontWeight: '700'
+      fontWeight: '700',
+      paddingBottom: 20,
     },
+
+    imageConstructors : {
+      width: 100, 
+      height: 100,
+    },
+
+    ImageView : {
+      flex : 1,
+      borderRadius: 14,
+      alignItems: 'center',
+      paddingRight: 6,
+      paddingTop: 16,
+    }
   })
         
