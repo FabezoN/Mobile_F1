cfb2de1ebdbe7488b1bcd59d6aa765f3b4fd11c5
Mathias COUTANT <mathias.coutant@pxcom.aero>
2023-12-11 16:07:54 +0100

    3 page

--- src/pages/DriversDetails.tsx
+++ src/pages/DriversDetails.tsx
@@ -1,7 +1,7 @@
 import { useRoute } from '@react-navigation/native';
 import moment from 'moment';
 import React, { useEffect, useState } from 'react';
-import { Text, View, ActivityIndicator, Image } from 'react-native';
+import { Text, View, ActivityIndicator, Image, StyleSheet, Animated } from 'react-native';
 import { driverImagesMapping } from '../components/DriverList.constants';
 
 
@@ -13,6 +13,8 @@ export function DriversDetails() {
   const points = route.params?.points;
   const [driverDetails, setDriverDetails] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  const [translateY] = useState(new Animated.Value(0));
   
   useEffect(() => {
     const fetchDriverDetails = async () => {
@@ -30,7 +32,19 @@ export function DriversDetails() {
       } finally {
         setLoading(false);
       }
-    };
+
+
+      const translateYAnimation = Animated.timing(translateY, {
+        toValue: -418, // Change this value to adjust the vertical translation
+        duration: 2000, // Animation duration in milliseconds
+        useNativeDriver: true, // Enable native driver for better performance
+      });
+
+      // Start the animation
+      translateYAnimation.start();
+
+
+    }
 
     if (driverId) {
       fetchDriverDetails();
@@ -44,17 +58,17 @@ export function DriversDetails() {
         <><React.Fragment>
           <Text style={{ fontWeight: '700', fontSize: 34, color: 'white', marginTop: 110, paddingLeft: 26 }}>{driverDetails.familyName}</Text>
           <Text style={{ fontWeight: '700', fontSize: 120, color: 'white', paddingLeft: 26, paddingTop: 110, lineHeight: 25 }}>{position}</Text>
-          <Image style={{ width: 400, height: 400, position: 'relative', bottom: 178, alignItems: 'center', justifyContent: 'center' }} source={driverImagesMapping[driverId]} />
+          <Animated.Image style={{ width: 400, height: 400, position: 'relative', top: 240, alignItems: 'center', justifyContent: 'center', transform: [{ translateY }] }} source={driverImagesMapping[driverId]} />
         </React.Fragment>
 
 
-        <View style={{ backgroundColor: '#1B1A19', height: 354, top: 500, position: 'absolute', width: '100%', borderTopRightRadius: 20, borderTopLeftRadius: 20 }}>
-          <Text style={{ color: 'white', fontWeight: '700', fontSize: 25, lineHeight: 21, top: 43, left: 26, width: 75, height: 30, justifyContent: 'center', alignItems: 'center' }}>{driverDetails.givenName}</Text>
-          <Text style={{color: 'white', textAlign: 'center', top: 36, position: 'absolute', right: 26, fontWeight: '700', fontSize: 25, width: 90}}>{points} PTS</Text>
+        <Animated.View style={{backgroundColor: '#1B1A19', height: 354, top: 500, position: 'absolute', width: '100%', borderTopRightRadius: 20, borderTopLeftRadius: 20 }}>
+          <Text style={{ color: 'white', fontWeight: '700', fontSize: 25, lineHeight: 21, top: 37, paddingTop: 10, left: 26, width: 105, height: 40, justifyContent: 'center', alignItems: 'center' }}>{driverDetails.givenName}</Text>
+          <Text style={{color: 'white', textAlign: 'center', top: 36, position: 'absolute', right: 26, fontWeight: '700', fontSize: 22, width: 90}}>{points} PTS</Text>
           <Text style={{ color: 'white', fontWeight: '700', fontSize: 40, lineHeight: 25, paddingLeft: 26, paddingTop: 50 }}>{driverDetails.familyName}</Text>
           <Text style={{ color: '#CCCDD7', paddingLeft: 26, fontWeight: '500', fontSize: 14, paddingTop: 10 }}>{moment().diff(driverDetails.dateOfBirth, 'years')} ans</Text>
           <Text style={{ color: '#CCCDD7', paddingLeft: 26, fontWeight: '500', fontSize: 14, paddingTop: 10 }}>{constructors}</Text>
-        </View></>
+        </Animated.View></>
       )}
     </View>
   );
